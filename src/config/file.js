64095a90beacfee72ac8a312be7507a0997841d4
María <mariagomezmateo97@gmail.com>
2024-10-21 18:34:15 +0200

    Cambios 21/10

--- src/config/file.js
+++ src/config/file.js
@@ -5,7 +5,7 @@ const connectCloudinary = () => {
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDUINARY_API_SECRET
+      api_secret: process.env.CLOUDINARY_API_SECRET
     })
     console.log('Conectados con Cloudinary. ¡Sube tus fotitos! :P')
   } catch (error) {
